df1073396bbd327a662d227451fbb25a162c3d33
dervism <dervis4@gmail.com>
2020-05-02 21:03:22 +0200

    Adding more tests

--- src/test/java/no/dervis/pokerhandkata/eval/PokerPatternEvaluatorTest.java
+++ src/test/java/no/dervis/pokerhandkata/eval/PokerPatternEvaluatorTest.java
@@ -4,7 +4,6 @@ import no.dervis.pokerhandkata.domain.Card;
 import no.dervis.pokerhandkata.domain.Hand;
 import org.junit.jupiter.api.Test;
 
-import static no.dervis.pokerhandkata.domain.CardType.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -12,74 +11,26 @@ class PokerPatternEvaluatorTest implements CardUtils {
 
     @Test
     void isStraight() {
-        Card[] hand = {
-                clubs(TWO),
-                clubs(THREE),
-                diamonds(FOUR),
-                hearts(FIVE),
-                spades(SIX)
-        };
-        assertTrue(PokerPatternEvaluator.isStraight(hand));
+        Card[] cards = textToHand("[2♣][3♣][4♦][5♥][6♠]").getCards();
+        assertTrue(PokerPatternEvaluator.isStraight(cards));
     }
 
     @Test
     void isNotStraight() {
-        Card[] hand = {
-                clubs(TWO),
-                clubs(THREE),
-                diamonds(FOUR),
-                hearts(FOUR),
-                spades(SIX)
-        };
-        assertFalse(PokerPatternEvaluator.isStraight(hand));
+        Card[] cards = textToHand("[2♣][3♣][4♦][4♥][6♠]").getCards();
+        assertFalse(PokerPatternEvaluator.isStraight(cards));
     }
 
     @Test
     void isHouse() {
-        Card[] hand = {
-                clubs(TWO),
-                hearts(TWO),
-                diamonds(TWO),
-                hearts(FIVE),
-                spades(FIVE)
-        };
-        assertTrue(PokerPatternEvaluator.isHouse(hand));
+        Card[] cards = textToHand("[2♣][2♥][2♦][5♥][5♠]").getCards();
+        assertTrue(PokerPatternEvaluator.isHouse(cards));
     }
 
     @Test
     void isNotHouse() {
-        Card[] hand = {
-                clubs(TWO),
-                hearts(TWO),
-                diamonds(NINE),
-                hearts(FIVE),
-                spades(FIVE)
-        };
-        assertFalse(PokerPatternEvaluator.isHouse(hand));
-    }
-
-    @Test
-    void isTwoPairs() {
-        Card[] cards = {
-                clubs(TWO),
-                hearts(TWO),
-                hearts(FIVE),
-                spades(FIVE),
-                diamonds(NINE)
-        };
-        assertTrue(PokerPatternEvaluator.isTwoPairs(Hand.of(cards)));
-    }
-
-    @Test
-    void isNotTwoPairs() {
-        Card[] cards = {
-                clubs(TWO),
-                hearts(TWO),
-                hearts(FIVE),
-                spades(SEVEN),
-                diamonds(NINE)
-        };
-        assertFalse(PokerPatternEvaluator.isTwoPairs(Hand.of(cards)));
+        Card[] cards = textToHand("[2♣][2♥][9♦][5♥][5♠]").getCards();
+        assertFalse(PokerPatternEvaluator.isHouse(cards));
     }
 
     @Test
@@ -93,8 +44,28 @@ class PokerPatternEvaluatorTest implements CardUtils {
     }
 
     @Test
+    void strictOnePair2() {
+        Card[] cards = textToHand("[8♦][9♠][J♠][J♣][K♠]").getCards();
+
+        assertTrue(PokerPatternEvaluator.isOnePair(cards));
+        assertTrue(PokerPatternEvaluator.isOnePairStrict(cards));
+        assertFalse(PokerPatternEvaluator.isTwoPairs(Hand.of(cards)));
+        assertFalse(PokerPatternEvaluator.isTwoPairsStrict(cards));
+    }
+
+    @Test
     void strictTwoPairs() {
         Hand hand = textToHand("[7♦][7♥][9♦][T♦][T♥]");
+
+        assertTrue(PokerPatternEvaluator.isTwoPairs(hand));
+        assertTrue(PokerPatternEvaluator.isTwoPairsStrict(hand.getCards()));
+        assertTrue(PokerPatternEvaluator.isOnePair(hand.getCards()));
+        assertTrue(PokerPatternEvaluator.isOnePairStrict(hand.getCards()));
+    }
+
+    @Test
+    void strictTwoPairs2() {
+        Hand hand = textToHand("[3♦][7♦][7♥][T♦][T♥]");
 
         assertTrue(PokerPatternEvaluator.isTwoPairs(hand));
         assertTrue(PokerPatternEvaluator.isTwoPairsStrict(hand.getCards()));
